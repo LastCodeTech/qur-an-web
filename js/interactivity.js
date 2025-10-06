c1668709c639d78c6a59541fdc4795ca67214f76
= <sulaimonabubakre260@gmail.com>
2025-10-06 23:27:01 +0100

    dhd

--- js/interactivity.js
+++ js/interactivity.js
@@ -1,7 +1,8 @@
 
   // Immediately-invoked function or DOMContentLoaded
-  document.addEventListener("DOMContentLoaded", () => {
+  
     const container = document.getElementById("surahContainer");
+   
 
     fetch("https://staticquran.vercel.app/api/v1/surah")
       .then(res => {
@@ -14,11 +15,12 @@
         const surahs = json.data;  // in the API docs, “data” holds the list of surah metadata
 
         surahs.forEach(surah => {
-          const card = document.createElement("div");
-          card.className = "w-80 bg-emerald-500 px-2 py-1 rounded-md shadow hover:shadow-lg transition border-1 border-emerald-700";
-
+         const card = document.createElement("div");
+          card.className = "w-80 bg-emerald-500 px-2 py-1 rounded-md shadow hover:shadow-lg transition border-2 border-yellow-300";
           // fill the card’s inner HTML
+
           card.innerHTML = `
+          <a href='new.html?seq=${surah.sequence}'>
             <div class="flex justify-between items-center">
               <h2 class="h-6 w-6 flex items-center justify-center bg-gray-300 rounded-md text-base font-semibold text-gray-700">
                 ${surah.sequence}
@@ -38,21 +40,36 @@
               <h2 class="text-base font-light verseNumber text-white">
                 ${surah.ayahCount} verses
               </h2>
-            </div>
+            </div></a>
           `;
 
-          container.appendChild(card);
+
+          if(card){
+             container.appendChild(card);
+          }
+         
+
+     
+        });   
         });
-      })
-      .catch(err => {
-        console.error("Failed to fetch surahs:", err);
-        container.innerHTML = `<p class="text-red-500">Could not load surahs.</p>`;
-      });
-  });
-//   async function getSurahReading(){
-//     const response = await fetch('');
-//     const data =await response.json();
-//   }
+     
+    //        document.addEventListener('load',function(){
+    // let index =  surah.sequence;
+    // let link ='https://staticquran.vercel.app/api/v1/surah/'+ index  
+    //  async function getsurah(){
+    //   const response = await fetch(link);
+    //   const data = response.json();
+    //   console.log(data);
+    //   let surahInfo= document.getElementById('surahInfo');
+    //   surahInfo.textContent ='helllo';
+     
+    //  }
+    //  getsurah()
+   
+  // })
+
+  
+  
 
 
 
